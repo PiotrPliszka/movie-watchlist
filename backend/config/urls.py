00100d7a3fd8bf1d64e544ca23820a3ba4f039c7
PiotrPliszka <181106@student.uwm.edu.pl>
2026-04-19 15:31:34 +0200

    Fix url

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -14,13 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     # adminpanel url
-    path('admin/', admin.site.urls),
-    
+    path("admin/", admin.site.urls),
     # movies app urls
-    path("movies/", include("movies/urls"))
+    path("api/", include("movies.urls")),
 ]
