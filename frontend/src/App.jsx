3961074c6115cd9e0acdbdb82ddd8f025c7c0900
PiotrPliszka <181106@student.uwm.edu.pl>
2026-04-20 17:27:36 +0200

    Fixing name and Routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { MovieList } from "./pages/MovieList";
+import { HomePage } from "./pages/HomePage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/movies" element={<MovieList />} />
-      <Route path="/movies/:id" element={<MovieDetails />} />
+      {/* <Route path="/movies/:id" element={<MovieDetails />} /> */}
     </Routes>
   );
 }
