02236532c5d9797d60a615712a70f25e093656da
PiotrPliszka <181106@student.uwm.edu.pl>
2026-04-22 10:54:46 +0200

    Update AddMovie

--- frontend/src/pages/AddMovie.jsx
+++ frontend/src/pages/AddMovie.jsx
@@ -5,6 +5,29 @@ import api from "../api/axios";
 import "./AddMovie.css";
 
 export function AddMovie() {
+  const [movie, setMovie] = useState({
+    title: "",
+    description: "",
+    release_year: "",
+    is_watched: false,
+  });
+  function handleChange(e) {
+    setMovie({ ...movie, [e.target.name]: e.target.value });
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    try {
+      const response = await api.post("movies/", movie);
+      console.log("Sukces: ", response.data);
+    } catch (error) {
+      if (error.response) {
+        console.error("Dane błędu: ", error.response.data);
+      } else {
+        console.error("Bład ogólny: ", error.message);
+      }
+    }
+  }
+
   return (
     <div className="form-container">
       <div className="form-topbar">
@@ -14,23 +37,47 @@ export function AddMovie() {
       </div>
       <form className="form">
         <label>Title</label>
-        <input type="text" />
-        <label>Description</label>
-        <textarea className="description-input" rows="10" />
-        <label>Relese year</label>
         <input
           type="text"
+          onChange={handleChange}
+          value={movie.title}
+          name="title"
+        />
+        <label>Description</label>
+        <textarea
+          className="description-input"
+          rows="10"
+          onChange={handleChange}
+          value={movie.description}
+          name="description"
+        />
+        <label>release_year</label>
+        <input
+          type="date"
           className="date-input"
           inputMode="numeric"
+          placeholder="YYYY-MM-DD"
           pattern="\d{4}-\d{2}-\d{2}"
           title="Format: YYYY-MM-DD"
+          onChange={handleChange}
+          value={movie.release_year}
+          name="release_year"
         />
         <label>Is watched</label>
         <label className="watched-row">
-          <input type="checkbox" />
+          <input
+            type="checkbox"
+            onChange={() =>
+              setMovie({ ...movie, is_watched: !movie.is_watched })
+            }
+            name="is_watched"
+            checked={movie.is_watched}
+          />
           <span>Mark as watched</span>
         </label>
-        <button type="submit">ADD</button>
+        <button type="submit" onClick={handleSubmit}>
+          ADD
+        </button>
       </form>
     </div>
   );
